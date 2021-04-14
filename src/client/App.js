39aa99063c6f4781b3d3e1e6d6fadf3bff570a81
DlnyaMazhari <mazharidlnya@gmail.com>
2021-04-14 16:39:38 +0200

    App.js is added, it contains the routing form

--- src/client/App.js
+++ src/client/App.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import TestComponent from "./components/TestComponent/TestComponent";
+import Home from "./components/Home";
+import MealDetails from "./components/MealDetails";
+import Meals from "./components/Meals";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import "./App.css";
 
 function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetchedData();
+  }, []);
+
+  const fetchedData = async function () {
+    const mealData = await fetch("/api/meals").then((resp) => resp.json());
+    setMeals(mealData);
+  };
   return (
     <Router>
-      <Route exact path="/">
-        <p>Hello</p>
-      </Route>
-      <Route exact path="/lol">
-        <p>lol</p>
-      </Route>
-      <Route exact path="/test-component">
-        <TestComponent></TestComponent>
-      </Route>
+      <div>
+        <Header />
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home meals={meals} />
+        </Route>
+
+        <Route exact path="/about">
+          <About />
+        </Route>
+
+        <Route exact path="/Meals">
+          <div>
+            <Meals meals={meals} />
+          </div>
+        </Route>
+
+        <Route exact path="/Meals/:id">
+          <div>
+            <MealDetails meals={meals} />
+          </div>
+        </Route>
+      </Switch>
+      <div>
+        <Footer />
+      </div>
     </Router>
   );
 }
